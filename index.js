579b99e247c2d32e038dd6b88d0339a9a31d3252
Jun Yuan <junyuan5816@gmail.com>
2025-10-22 15:25:13 -0400

    Test 1

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-const express = require("express");
-const socketIO = require("socket.io");
-const path = require("path");
-const PORT = process.env.PORT || 3000;
+ const express = require("express");
+ const socketIO = require("socket.io");
+ const path = require("path");
+ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
+const CHEFPATH = path.join(__dirname, 'chefPage.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
- .listen(PORT, () => console.log("Listening on localhost" + PORT));
+ const server = express()
+   .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log("Listening on localhost" + PORT));
 
-const io = socketIO(server);
+ const io = socketIO(server);
 
-io.on("connection", function(socket) {
-  socket.on("join", function (room) {
-    socket.join(room)
-    socket.on("image", function(msg) {
-      socket.broadcast.to(room).emit("image", msg);
-    });
-  })
-});
+ io.on("connection", function(socket) {
+   socket.on("join", function (room) {
+     socket.join(room)
+     socket.on("image", function(msg) {
+       socket.broadcast.to(room).emit("image", msg);
+     });
+   })
+ });
+
+
+
+
+//const express = require('express'); //requires express module
+//const socket = require('socket.io'); //requires socket.io module
+//const fs = require('fs');
+//const app = express();
+//var PORT = process.env.PORT || 3000;
+//const server = app.listen(PORT); //tells to host server on localhost:3000
+
+
+////Playing variables:
+//app.use(express.static('public')); //show static files in 'public' directory
+//console.log('Server is running');
+//const io = socket(server);
+
+//var count = 0;
+
+
+////Socket.io Connection------------------
+//io.on('connection', (socket) => {
+
+//    console.log("New socket connection: " + socket.id)
+
+//    socket.on('counter', () => {
+//        count++;
+//        console.log(count)
+//        io.emit('counter', count);
+//    })
+//})
